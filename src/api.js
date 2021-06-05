e86f4e9f7344b78e6eb1a78e00226aca7606ab17
francisco <francisco@trend2b.com>
2021-06-04 21:07:01 -0300

    add env var

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   contentType: 'application/json',
-  baseURL: "http://127.0.0.1:3001"
+  baseURL: process.env.REACT_APP_URL_API ?? "http://127.0.0.1:3001"
 })
 
 export default api
